8436b5185cae8360284bf261606935b76e45d9d1
MaximeT-ACMT <mtalbot2009@gmail.com>
2026-02-19 23:16:39 -0500

    Q5

--- exercices/module 3/Tache 03/Q5.py
+++ exercices/module 3/Tache 03/Q5.py
@@ -1,5 +1,5 @@
 import math
 def calculate_circle_area(diameter):
-radius = diameter / 2
-area = math.pi * radius * 2
+ radius = diameter / 2
+ area = math.pi * radius ** 2
 print (calculate_circle_area)
