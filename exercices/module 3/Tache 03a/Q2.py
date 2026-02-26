b59d3ed1c8360b68abbcaf1b4bc5be38083a488e
MaximeT-ACMT <mtalbot2009@gmail.com>
2026-02-25 23:04:50 -0500

    Q2

---
+++ exercices/module 3/Tache 03a/Q2.py
@@ -0,0 +1,6 @@
+# Demander pour un nombre au utilisateur
+nombre = int(input("Écrire un nombre: "))
+# Faire la multiplication avec le numéro 5
+result = nombre * 5
+# Montrer le résultat
+print("Le résultat est:",result)
