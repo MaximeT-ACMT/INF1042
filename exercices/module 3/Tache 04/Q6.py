e43c2d704ec7d81bc960aa0833ba5f0ff9258d4c
MaximeT-ACMT <mtalbot2009@gmail.com>
2026-03-06 13:03:11 -0500

    Q6

---
+++ exercices/module 3/Tache 04/Q6.py
@@ -0,0 +1,10 @@
+Longeur1 = float(input("Longueur rectangle 1: "))
+Largeur1 = float(input("Largeur rectangle 1: "))
+
+Longeur2 = float(input("Longueur rectangle 2: "))
+Largeur2 = float(input("Largeur rectangle 2: "))
+
+aire1 = Longeur1 * Largeur1
+aire2 = Longeur2 * Largeur2
+
+print(aire1 == aire2)
