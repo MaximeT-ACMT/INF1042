51e03cbeceb59e6ac3369160cb8d8c88e52a4d19
MaximeT-ACMT <mtalbot2009@gmail.com>
2026-03-29 15:23:39 -0400

    Q2

---
+++ sommative/tâche_02/Q2.py
@@ -0,0 +1,13 @@
+# Maxime Talbot
+# Converting Celsius temperature into Fahrenheit and Kelvin.
+
+celsius = float(input("Write Celsius temperature: "))
+20
+fahrenheit = (celsius * 9 / 5) + 32
+kelvin = celsius + 273.15
+
+print(f"{celsius}°C is equal to {fahrenheit}°F and {kelvin}K")
+
+# Float makes text become decimal numbers to use for operations. 
+# Then we caculate Fahrenheit and Kelvin temperature like you mentionned.
+# Then we print Fahrenheit and Kelvin units depending on what Celsius is using f Strings.
