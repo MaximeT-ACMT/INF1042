13c1ef0fcf4f6556199a41c802970aeb418f2c8d
MaximeT-ACMT <mtalbot2009@gmail.com>
2026-03-29 15:15:02 -0400

    Q1

---
+++ sommative/tâche_02/Q1.py
@@ -0,0 +1,9 @@
+# Maxime Talbot Question 1
+# Calculate the average number of days in a month for normal and leap years.
+avg_year = 365 / 12
+avg_leap_year = 366 / 12
+
+print(f"Average days per month (Normal Year): ({avg_year:.2f}")
+print(f"Average days per month (Leap Year): {avg_leap_year:.2f}")
+
+# Uses F strings (f) to display the averages for a leap year and a normal year.
