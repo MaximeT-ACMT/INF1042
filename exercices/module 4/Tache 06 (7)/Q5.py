ef79c6b498ed490d381fab37aa4b0dbaa7ed768f
MaximeT-ACMT <mtalbot2009@gmail.com>
2026-04-09 22:08:35 -0400

    Q5

---
+++ exercices/module 4/Tache 06 (7)/Q5.py
@@ -0,0 +1,6 @@
+eleves_A = {"Mia", "Noah", "Liam", "Zoe"}
+eleves_B = {"Zoe", "Emma", "Liam", "Olivier"}
+
+print(f"Dans A seulement : {eleves_A - eleves_B}")
+
+print(f"Différence symétrique : {eleves_A ^ eleves_B}")
